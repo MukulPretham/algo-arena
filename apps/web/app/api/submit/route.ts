1e586018c763bd527b89297d323febabe8f21e06
Mukul Pretham <mukulpretham@gmail.com>
2025-04-28 22:21:25 +0530

    some backend

---
+++ apps/web/app/api/submit/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs"
+import { client } from "@repo/db/client"
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    if (!req.body) {
+        return NextResponse.json({ error: "bad request" })
+    }
+
+    if (!body) {
+        return NextResponse.json({ error: "bad request" })
+    }
+    if (!body.code || !body.lang || !body.problemId) {
+        return NextResponse.json({ error: "bad request" })
+    }
+    console.log(body?.problemId);
+    try {
+        const currProblem = await client.problem.findFirst({
+            where: {
+                id: body?.problemId
+            }
+        });
+        
+        const inputsDir = fs.readdirSync(`../problems/${currProblem?.title}/inputs`);
+        const inputs: any = [];
+
+        inputsDir.forEach(file => {
+            const content = fs.readFileSync(`../problems/${currProblem?.title}/inputs/${file}`);
+            inputs.push(content);
+        })
+
+        const outputsDir = fs.readdirSync(`../problems/${currProblem?.title}/outputs`);
+        const outputs: any = [];
+
+        outputsDir.forEach(file => {
+            const content = fs.readFileSync(`../problems/${currProblem?.title}/outputs/${file}`);
+            outputs.push(content);
+        })
+
+        return NextResponse.json({
+            inputs: inputs,
+            outputs: outputs
+        });
+
+    } catch (err) {
+        return NextResponse.json({
+            error: err
+        })
+    }
+    
+}
--- apps/web/components/Code.tsx
+++ apps/web/components/Code.tsx
@@ -1,33 +1,49 @@
 "use client"
-import React from 'react';
+import React, { useRef } from 'react';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-c';   // first import C
+import 'prismjs/components/prism-cpp'; // then import C++
+
 import 'prismjs/themes/prism.css';
 
 const Code = () => {
   const [code, setCode] = React.useState(
     `function add(a, b) {\n  return a + b;\n}`
   );
+  const langRef = useRef<HTMLSelectElement>(null);
+  const selectedLanguage = langRef.current?.value || "javascript"; // fallback to 'javascript'
+  const languageGrammar = languages[selectedLanguage];
 
   return (
     <div style={{
       width: "50%"
     }}>
+      <select ref={langRef} name="" id="">
+        <option value="javascript">Javascript</option>
+        <option value="Java">Java</option>
+        <option value="C++">C++</option>
+      </select>
       <Editor
         value={code}
         onValueChange={(newCode) => setCode(newCode)}
         highlight={(code) => {
           if (languages.js) {
-            return highlight(code, languages.js,'javascript');
+            //@ts-ignore
+            return highlight(code, langRef.current?.value === "javascript" ? languages.js : languages[selectedLanguage], `${langRef.current?.value}`);
           }
           return code; // Return the code without highlighting if languages.js is undefined
         }}
         padding={10}
         style={{
+          backgroundColor: "white",
+
           fontFamily: '"Fira code", "Fira Mono", monospace',
+          marginTop: "4px",
+          marginRight: "4px",
           fontSize: 15,
           height: "60vh",
           overflow: "scroll",
@@ -37,19 +53,20 @@ const Code = () => {
       />
       <div>
         <button
-        onClick={()=>{
-          console.log(code);
-        }}
-         style={{
-          backgroundColor: "green",
-          color: "white",
-          padding: "7px",
-          borderRadius: "14px",
-          cursor: "pointer",
-          position: "fixed",
-          marginTop:"12px",
-          right: "5px"
-        }}>Submit</button>
+          onClick={() => {
+            console.log(langRef.current?.value);
+          }}
+          style={{
+            backgroundColor: "#4fbf31",
+            color: "white",
+            padding: "10px 10px",
+            borderRadius: "14px",
+            cursor: "pointer",
+            position: "sticky",
+            border: "2px solid black",
+            marginTop: "1px",
+            right: "5px"
+          }}>Submit</button>
       </div>
     </div>
 
