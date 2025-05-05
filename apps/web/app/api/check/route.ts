77793b5a4bbcc835165e1e5fc405c010e0d02376
Mukul Pretham <mukulpretham@gmail.com>
2025-05-05 21:41:27 +0530

    modified check endpoint

--- apps/web/app/api/check/route.ts
+++ apps/web/app/api/check/route.ts
@@ -28,51 +28,89 @@ export async function POST(req: NextRequest) {
         const results = await Promise.all(resultsPromises);
         console.log(results);
 
-        for (const result of results) {
-            if (result.status.description === "Wrong Answer") {
-                await client.submissions.update({
-                    where: {
-                        id: submissionId
-                    },
-                    data: {
-                        status: "Wrong Answer"
-                    }
-                });
-                currSubmission =  await client.submissions.findFirst({
-                    where:{
-                        id: id
-                    }
-                });
-                return NextResponse.json(results);
-                
-            };
-            
-            if (result.status.description === "Accepted") {
-                await client.submissions.update({
-                    where: {
-                        id: submissionId
-                    },
-                    data: {
-                        status: "Accepted"
-                    }
-                });
-            };
-            if (result.status.description === "Processing") {
-                await client.submissions.update({
-                    where: {
-                        id: submissionId
-                    },
-                    data: {
-                        status: "Processing"
-                    }
-                });
-            };
-        }
-        currSubmission =  await client.submissions.findFirst({
-            where:{
-                id: id
-            }
+        const finalResults = results.map((entry)=>{
+            return entry.status.description
         });
+
+        const resultLemgth = finalResults.length;
+        let resultCounter = 0;
+        let processingCounter = 0;
+        for (const result of finalResults) {
+            if(result === "Accepted"){
+                resultCounter++;
+            }
+            if(result === "Processing"){
+                processingCounter++;
+            }
+        }
+        if(processingCounter > 0){
+            await client.submissions.update({
+                where: {
+                    id: submissionId
+                },
+                data: {
+                    status: "Processing"
+                }
+            });
+            
+        }
+        if(resultLemgth === resultCounter){
+            await client.submissions.update({
+                where: {
+                    id: submissionId
+                },
+                data: {
+                    status: "Accepted"
+                }
+            });
+            
+        }
+
+        // for (const result of results) {
+        //     if (result.status.description === "Wrong Answer") {
+        //         await client.submissions.update({
+        //             where: {
+        //                 id: submissionId
+        //             },
+        //             data: {
+        //                 status: "Wrong Answer"
+        //             }
+        //         });
+        //         currSubmission =  await client.submissions.findFirst({
+        //             where:{
+        //                 id: id
+        //             }
+        //         });
+        //         return NextResponse.json(results);
+                
+        //     };
+            
+        //     if (result.status.description === "Accepted") {
+        //         await client.submissions.update({
+        //             where: {
+        //                 id: submissionId
+        //             },
+        //             data: {
+        //                 status: "Accepted"
+        //             }
+        //         });
+        //     };
+        //     if (result.status.description === "Processing") {
+        //         await client.submissions.update({
+        //             where: {
+        //                 id: submissionId
+        //             },
+        //             data: {
+        //                 status: "Processing"
+        //             }
+        //         });
+        //     };
+        // }
+        // currSubmission =  await client.submissions.findFirst({
+        //     where:{
+        //         id: id
+        //     }
+        // });
         
         // return NextResponse.json({
         //     STATUS: currSubmission?.status
